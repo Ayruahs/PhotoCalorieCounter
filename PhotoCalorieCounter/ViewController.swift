55fee39244b5df63d961ff05181a034105315807
Shaurya Sinha <ShauryaSinha@ASHUTOSHs-MacBook-Pro.local>
2017-08-03 23:17:38 -0700

    Changed classifier from default to food and updated UI

--- PhotoCalorieCounter/ViewController.swift
+++ PhotoCalorieCounter/ViewController.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 import VisualRecognitionV3
+import SVProgressHUD
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var mealInfoLabel: UILabel!
     
     let imagePicker = UIImagePickerController()
     //Get API key and version from another file, not included in git tracking
     let api = ApiInfo()
-    
-    
-    var classificationResults: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,10 +30,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             
+            SVProgressHUD.show()
+            
             imageView.image =  image
             imagePicker.dismiss(animated: true, completion: nil)
             
             let imageData = UIImageJPEGRepresentation(image, 0.01)
+            
+            var foodDict = Dictionary<Double, String>()
             
             let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
             
@@ -44,30 +47,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             let foodClassifier = VisualRecognition(apiKey: api.apiKey, version: api.version)
             
-            
-            foodClassifier.classify(imageFile: fileURL, success: { (classifiedImages) in
-                print(classifiedImages)
+            foodClassifier.classify(imageFile: fileURL, classifierIDs: ["food"], language: "en", success: { (classifiedImages) in
+                //print(classifiedImages)
                 let possibleClasses = classifiedImages.images.first!.classifiers.first!.classes
                 
-                self.classificationResults = []
-                
                 for i in 0..<possibleClasses.count {
-                    self.classificationResults.append(possibleClasses[i].classification)
+                    foodDict[possibleClasses[i].score] = possibleClasses[i].classification
                 }
-                // Figure out how to use this:
-                let googleUrl = "https://www.google.com/search?q=hotdog+calories&oq=hotdog+calories"
-                let url = NSURL(string: googleUrl)
-                URLSession.shared.dataTask(with: url! as URL) {
-                    (data, response, error) in
-                    // deal with error etc accordingly       
-                    print("Data: \n")
-                    print(data as Any)
-                    print("response: \n")
-                    print(response as Any)
-                    print("error: \n")
-                    print(error as Any)
-                    
-                }.resume()
+        
+                let mostProbableFood = foodDict[foodDict.keys.max()!]
+                DispatchQueue.main.async {
+                    self.mealInfoLabel.text = "Your meal is: " + mostProbableFood!
+                }
+                
+                SVProgressHUD.dismiss()
                 
             })
             
